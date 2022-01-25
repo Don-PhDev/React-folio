7e39464825975a280be020a37f87ff45e7ed8aac
Raymond <raymond@morphos.is>
2022-01-26 06:06:25 +0800

    Set up body content components

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,5 +1,29 @@
+import About from "./About/index";
+import Skills from "./Skills/index";
+import Projects from "./Projects/index";
+import Contact from "./Contact/index";
+import "./body.css";
+
 function Body() {
-  return <div>This is the body</div>;
+  return (
+    <div className="body">
+      <section id="about">
+        <About />
+      </section>
+
+      <section id="skills">
+        <Skills />
+      </section>
+
+      <section id="projects">
+        <Projects />
+      </section>
+
+      <section id="contact">
+        <Contact />
+      </section>
+    </div>
+  );
 }
 
 export default Body;
