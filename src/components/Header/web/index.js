385e853c40de6ed51a10eb7b47eba7b1061082e0
Raymond <raymond@morphos.is>
2022-01-26 04:30:58 +0800

    Correct className

--- src/components/Header/web/index.js
+++ src/components/Header/web/index.js
@@ -3,25 +3,25 @@ import "./web.css";
 function Web() {
   return (
     <div className="web">
-      <div className="web-links">
+      <div className="web-link">
         <a href="#about">
           <i class="fi fi-rr-info link-icon"></i>About
         </a>
       </div>
 
-      <div className="web-links">
+      <div className="web-link">
         <a href="#skills">
           <i class="fi fi-rr-list-check link-icon"></i>Skills
         </a>
       </div>
 
-      <div className="web-links">
+      <div className="web-link">
         <a href="#projects">
           <i class="fi fi-rr-copy-alt link-icon"></i>Projects
         </a>
       </div>
 
-      <div className="web-links">
+      <div className="web-link">
         <a href="#contact">
           <i class="fi fi-rr-user link-icon"></i>Contact
         </a>
