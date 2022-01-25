a070e8180b61bc86fdae3b82ee0f12135a69edf4
Raymond <raymond@morphos.is>
2022-01-26 02:41:42 +0800

    Responsive header for large screens

--- src/components/Header/web/index.js
+++ src/components/Header/web/index.js
@@ -1,7 +1,33 @@
 import "./web.css";
 
 function Web() {
-  return <div>This is web</div>;
+  return (
+    <div className="web">
+      <div className="web-links">
+        <a href="#about">
+          <i class="fi fi-rr-info link-icon"></i>About
+        </a>
+      </div>
+
+      <div className="web-links">
+        <a href="#skills">
+          <i class="fi fi-rr-list-check link-icon"></i>Skills
+        </a>
+      </div>
+
+      <div className="web-links">
+        <a href="#projects">
+          <i class="fi fi-rr-copy-alt link-icon"></i>Projects
+        </a>
+      </div>
+
+      <div className="web-links">
+        <a href="#contact">
+          <i class="fi fi-rr-user link-icon"></i>Contact
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Web;
