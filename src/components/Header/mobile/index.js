de12b0de899bd24f775045162f55340b859496ef
Raymond <raymond@morphos.is>
2022-01-26 04:31:43 +0800

    Responsive header for small screens

--- src/components/Header/mobile/index.js
+++ src/components/Header/mobile/index.js
@@ -1,7 +1,38 @@
 import "./mobile.css";
 
-function Mobile() {
-  return <div>This is mobile</div>;
+function Mobile({ isOpen, setIsOpen }) {
+  return (
+    <div className="mobile">
+      <div className="close-icon" onClick={() => setIsOpen(!isOpen)}>
+        <i class="fi fi-rr-cross"></i>
+      </div>
+      <div className="mobile-links">
+        <div className="mobile-link">
+          <a href="#about">
+            <i class="fi fi-rr-info link-icon"></i>About
+          </a>
+        </div>
+
+        <div className="mobile-link">
+          <a href="#skills">
+            <i class="fi fi-rr-list-check link-icon"></i>Skills
+          </a>
+        </div>
+
+        <div className="mobile-link">
+          <a href="#projects">
+            <i class="fi fi-rr-copy-alt link-icon"></i>Projects
+          </a>
+        </div>
+
+        <div className="mobile-link">
+          <a href="#contact">
+            <i class="fi fi-rr-user link-icon"></i>Contact
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Mobile;
