53ca6bd0adccea8e12c8f615f52a7e8d102bdbd5
Raymond <raymond@morphos.is>
2022-01-27 01:28:09 +0800

    About section

--- src/components/Body/About/index.js
+++ src/components/Body/About/index.js
@@ -1,7 +1,30 @@
 import "./about.css";
 
 function About() {
-  return <div>This is About page</div>;
+  return (
+    <div className="about">
+      <div className="about-top">
+        <div className="about-left">
+          <div className="about-text">
+            Hi 👋, I'm <span className="about-name">Don Forrest Usbal</span>
+            <br />
+            a 19-year-old from the Philippines. <br />
+            I've been programming since I was 16 years old, <br />
+            and up until now, my enjoyment and excitement <br />
+            seeing my programs work hasn't changed.
+          </div>
+        </div>
+        <div className="about-right">
+          <img
+            src={require("../../../assets/programming.svg").default}
+            alt="Programming illustration"
+            className="about-image"
+          />
+        </div>
+      </div>
+      <div className="about-bottom"></div>
+    </div>
+  );
 }
 
 export default About;
