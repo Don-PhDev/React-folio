b65e6f37a0e97846bf28c5bd7e719769fa360a7c
Raymond <raymond@morphos.is>
2022-01-27 04:21:44 +0800

    Organize socmed data

---
+++ src/components/data/social.js
@@ -0,0 +1,20 @@
+export const SocialData = [
+  {
+    title: "Facebook icon",
+    platform: "Facebook",
+    icon: require("../../assets/facebook.png"),
+    link: "https://www.facebook.com/dfusbal",
+  },
+  {
+    title: "LinkedIn icon",
+    platform: "LinkedIn",
+    icon: require("../../assets/linkedin.png"),
+    link: "https://www.linkedin.com/in/don-forrest-usbal/",
+  },
+  {
+    title: "Github icon",
+    platform: "Github",
+    icon: require("../../assets/github.png"),
+    link: "https://github.com/ducreations",
+  },
+];
